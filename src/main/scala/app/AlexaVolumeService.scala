ef0bd581eb33b28961a0879bd544817339d44033
ReactivePatterns <stanlea10@gmail.com>
2016-08-23 17:58:42 -0400

    refactored try

--- src/main/scala/app/AlexaVolumeService.scala
+++ src/main/scala/app/AlexaVolumeService.scala
@@ -19,24 +19,24 @@ object AlexaVolumeService extends VolumeService[String, AlexaVolume] {
   val up: Int => AlexaVolume = { l => AlexaVolumes(l + 1)}
   val down: Int => AlexaVolume = { l => AlexaVolumes(l - 1)}
 
-  def processTry[A](thunk: => A, sideEffect: A => Unit, message: String): Try[A] = {
+  def process[A](thunk: => A, sideEffect: A => Unit, message: String): Try[A] = {
     val t = Try { thunk }
     t.foreach(sideEffect)
     t.transform(v => Success(v), t => Failure(new Error(message)))
   }
 
   override def set(level: String): Try[AlexaVolume] = {
-    processTry[AlexaVolume]({ AlexaVolumes(level.toInt) }, state_=,
+    process[AlexaVolume]({ AlexaVolumes(level.toInt) }, state_=,
       "The volume must be a whole number between 0 and 10")
   }
 
   override def louder(): Try[AlexaVolume] = {
-    processTry[AlexaVolume]({ state.flatMap(up) }, state_=,
+    process[AlexaVolume]({ state.flatMap(up) }, state_=,
       "Cannot go higher")
   }
 
   override def lower(): Try[AlexaVolume] = {
-    processTry[AlexaVolume]({ state.flatMap(down) }, state_=,
+    process[AlexaVolume]({ state.flatMap(down) }, state_=,
       "Cannot go lower")
   }
 }
