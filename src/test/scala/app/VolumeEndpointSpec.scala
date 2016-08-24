71805ebed7572c448ae6f04de61f627f22371146
ReactivePatterns <stanlea10@gmail.com>
2016-08-24 17:36:12 -0400

    tests

--- src/test/scala/app/VolumeEndpointSpec.scala
+++ src/test/scala/app/VolumeEndpointSpec.scala
@@ -6,16 +6,34 @@ import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.scalatest._
 
-class VolumeEndpointSpec extends FlatSpec with Matchers with ScalatestRouteTest with VolumeUserService {
+class VolumeEndpointSpec extends WordSpec with ShouldMatchers with ScalatestRouteTest with VolumeUserService {
   override def testConfigSource = "akka.loglevel = WARNING"
   override def config = testConfig
   val logger = NoLogging
 
-  "Endpoint" should "respond to a valid level submission" in {
-    Post(s"/volume/set", SetVolume("7")) ~> routes ~> check {
-      status shouldBe OK
-      contentType shouldBe `application/json`
-      responseAs[VolumeChanged].change.level shouldBe 7
+  "Endpoint" should {
+    "respond to a valid level submission" in {
+      Post(s"/volume/set", SetVolume("7")) ~> routes ~> check {
+        status shouldBe OK
+        contentType shouldBe `application/json`
+        responseAs[VolumeChanged].change.level shouldBe 7
+      }
+    }
+
+    "respond to an invalid level submission" in {
+      Post(s"/volume/set", SetVolume("7.5")) ~> routes ~> check {
+        status shouldBe InternalServerError
+        contentType shouldBe `application/json`
+        responseAs[VolumeNotChanged].info shouldBe Constants.LevelValidationMessage
+      }
+    }
+
+    "respond to a valid up submission" in {
+      Post(s"/volume/up", VolumeUp) ~> routes ~> check {
+        status shouldBe OK
+        contentType shouldBe `application/json`
+        responseAs[VolumeChanged].change.level shouldBe 8
+      }
     }
   }
 }
